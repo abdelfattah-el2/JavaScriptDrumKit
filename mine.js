8c7e2270942ed3b60a322dd06259e8f8863c10b7
abdelfattah-el2 <abdelnasserwaled400@gmil.com>
2023-12-12 21:53:16 +0200

    end fix the code

--- mine.js
+++ mine.js
@@ -3,10 +3,7 @@
 window.addEventListener("keydown" , function(e){
  
    const auide = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-   const Key = document.querySelector(`div[data-key="${e.keyCode}"]`)
-
    if(!auide) return
    auide.currentTime = 0
     auide.play()
-    
 })
